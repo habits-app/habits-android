a90cf9e2553af5378450e765452fa53cd8bc023d
Florian Schuster <mail@florianschuster.at>
2020-02-25 14:21:04 +0100

    Updated viewbinding delegates

--- app/src/main/kotlin/com/habits/app/ext/ViewViewBindingDelegate.kt
+++ app/src/main/kotlin/com/habits/app/ext/ViewViewBindingDelegate.kt
@@ -1,9 +1,9 @@
 package com.habits.app.ext
 
 import android.view.LayoutInflater
-import androidx.appcompat.app.AppCompatActivity
+import android.view.View
 import androidx.viewbinding.ViewBinding
 
-inline fun <B : ViewBinding> AppCompatActivity.viewBinding(
+inline fun <B : ViewBinding> View.viewBinding(
     crossinline bindingInflater: (LayoutInflater) -> B
-) = lazy { bindingInflater(layoutInflater) }
+) = bindingInflater(LayoutInflater.from(context))
--- app/src/main/kotlin/com/habits/app/overview/OverviewAdapter.kt
+++ app/src/main/kotlin/com/habits/app/overview/OverviewAdapter.kt
@@ -1,11 +1,11 @@
 package com.habits.app.overview
 
-import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
 import com.habits.app.databinding.ItemOverviewBinding
+import com.habits.app.ext.viewBinding
 import com.habits.models.Habit
 import kotlinx.coroutines.channels.BroadcastChannel
 import kotlinx.coroutines.flow.Flow
@@ -20,7 +20,7 @@ internal class OverviewAdapter : ListAdapter<Habit, HabitViewHolder>(diffUtilIte
     val interaction: Flow<OverviewAdapterInteraction> get() = _interaction.asFlow()
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): HabitViewHolder =
-        HabitViewHolder(ItemOverviewBinding.inflate(LayoutInflater.from(parent.context)))
+        HabitViewHolder(parent.viewBinding(ItemOverviewBinding::inflate))
 
     override fun onBindViewHolder(holder: HabitViewHolder, position: Int) =
         holder.bind(getItem(position)) { _interaction.offer(it) }
@@ -41,9 +41,7 @@ internal class HabitViewHolder(
 ) : RecyclerView.ViewHolder(binding.root) {
 
     fun bind(habit: Habit, interaction: (OverviewAdapterInteraction) -> Unit) {
-        binding.root.setOnClickListener {
-            interaction(OverviewAdapterInteraction.HabitClicked(habit))
-        }
+        binding.root.setOnClickListener { interaction(OverviewAdapterInteraction.HabitClicked(habit)) }
         binding.itemOverViewTitle.text = habit.toString()
     }
 }
